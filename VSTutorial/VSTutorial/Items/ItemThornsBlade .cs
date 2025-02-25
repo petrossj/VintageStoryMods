15bb8be7fc6ff798d269d7f3646dd1f1115eaeda
Jeff.Petross <jeffpetross@gmail.com>
2025-02-24 19:36:06 -0600

    finishing basic code tutorials

---
+++ VSTutorial/VSTutorial/Items/ItemThornsBlade .cs
@@ -0,0 +1,41 @@
+﻿using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+
+namespace VSTutorial.Items
+{
+    internal class ItemThornsBlade : Item
+    {
+        public override void OnAttackingWith(IWorldAccessor world, Entity byEntity, Entity attackedEntity, ItemSlot itemslot)
+        {
+            //api.Logger.Event("Got attack with thorns blade!");
+
+            DamageSource damage = new DamageSource()
+            {
+                Type = EnumDamageType.PiercingAttack,
+                SourceEntity = byEntity,
+                KnockbackStrength = 0
+            };
+
+            DamageSource healing = new DamageSource()
+            {
+                Type = EnumDamageType.Heal,
+                SourceEntity = byEntity,
+                KnockbackStrength = 0
+            };
+
+            //byEntity.ReceiveDamage(damage, 0.25f);
+            byEntity.ReceiveDamage(healing, 1.0f);
+
+            if (attackedEntity.Alive)
+            {
+                // take damage on hitting live target
+                //byEntity.ReceiveDamage(damage, 0.25f);
+
+                //heal on hit living target
+                //byEntity.
+            }
+
+            base.OnAttackingWith(world, byEntity, attackedEntity, itemslot);
+        }
+    }
+}
---
+++ VSTutorial/VSTutorial/VSTutorialModSystem.cs
@@ -0,0 +1,32 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Server;
+using Vintagestory.API.Config;
+using Vintagestory.API.Common;
+using VSTutorial.Blocks;
+using VSTutorial.Items;
+
+namespace VSTutorial;
+
+public class VSTutorialModSystem : ModSystem
+{
+
+    // Called on server and client
+    // Useful for registering block/entity classes on both sides
+    public override void Start(ICoreAPI api)
+    {
+        api.RegisterBlockClass($"{Mod.Info.ModID}.trampoline", typeof(BlockTrampoline));
+        api.RegisterItemClass($"{Mod.Info.ModID}.thornsblade", typeof(ItemThornsBlade));
+        //Mod.Logger.Notification("Hello from template mod: " + api.Side);
+    }
+
+    //public override void StartServerSide(ICoreServerAPI api)
+    //{
+    //    Mod.Logger.Notification($"Hello from template mod server side: {Lang.Get("vstutorial:hello")}");
+    //}
+
+    //public override void StartClientSide(ICoreClientAPI api)
+    //{
+    //    Mod.Logger.Notification($"Hello from template mod client side: {Lang.Get("vstutorial:hello")}");
+    //}
+
+}
