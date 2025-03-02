9ccc2814255ef81f4be5721f8510b1a7200ffceb
Jeff.Petross <jeffpetross@gmail.com>
2025-03-02 17:03:02 -0600

    adding blank project

---
+++ TheChosenOne/TheChosenOne/TheChosenOneModSystem.cs
@@ -0,0 +1,26 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Server;
+using Vintagestory.API.Config;
+using Vintagestory.API.Common;
+
+namespace TheChosenOne;
+
+public class TheChosenOneModSystem : ModSystem
+{
+    // Called on server and client
+    // Useful for registering block/entity classes on both sides
+    public override void Start(ICoreAPI api)
+    {
+        Mod.Logger.Notification("Hello from template mod: " + api.Side);
+    }
+
+    public override void StartServerSide(ICoreServerAPI api)
+    {
+        Mod.Logger.Notification("Hello from template mod server side: " + Lang.Get("thechosenone:hello"));
+    }
+
+    public override void StartClientSide(ICoreClientAPI api)
+    {
+        Mod.Logger.Notification("Hello from template mod client side: " + Lang.Get("thechosenone:hello"));
+    }
+}
